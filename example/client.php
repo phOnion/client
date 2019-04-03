65953e68928cfea84a99ec986e592ce7a036ac5b
DaGhostman <ddimitrov.dd@gmail.com>
2019-04-03 19:24:27 +0300

    updated example

--- example/client.php
+++ example/client.php
@@ -1,14 +1,12 @@
 <?php
 
-use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Stream\StreamInterface;
 use Onion\Framework\Client\Client;
 use function Onion\Framework\EventLoop\loop;
-use function Onion\Framework\EventLoop\detach;
+use GuzzleHttp\Stream\Stream;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$request = new Request('GET', 'https://example.com');
 loop();
 $symbols = [];
 $buffer = [];
@@ -16,45 +14,15 @@ stream_set_blocking(STDIN, false);
 $client = new Client(Client::TYPE_TCP, 'localhost:1338');
 $client->on('connect', function (StreamInterface $stream) {
     echo "Connected\n";
-    // $stream->write("Test");
+    $stream->write("Test");
 });
 $client->on('data', function (StreamInterface $__stream) use (&$symbols, &$buffer) {
-    // $pre = [];
-    // $pre = array_keys(get_defined_vars());
-    // extract($symbols);
-    // $code = $__stream->getContents();
-
-    // if (preg_match('~^:(\w+)\s(.*)?$~i', $code, $matches)) {
-    //     switch($matches[1]) {
-    //         case 'save':
-    //             file_put_contents($matches[2], "<?php\n" . implode("\n", $buffer));
-    //             break;
-    //         case 'quit':
-    //         echo "Bye!\n";
-    //             break;
-    //         default:
-    //             echo "Unknown command\n";
-    //             break;
-    //     }
-
-    //     return;
-    // }
-    // $buffer[] = $code;
-    // echo eval($code);
-    // $post = get_defined_vars();
-    // foreach ($pre as $name) {
-    //     if (isset($post[$name])) {
-    //         unset($post[$name]);
-    //     }
-    // }
-
-    // $symbols = $post;
-
-    echo $__stream->getContents();
+    echo "> {$__stream->read(8192)}";
 });
 $client->on('close', function () {
     echo "Bye!\n";
 });
+$client->proxy(new Stream(STDIN));
 $client->connect()
     ->otherwise(function (\Throwable $exception) {
         echo "Error: {$exception->getMessage()}";
