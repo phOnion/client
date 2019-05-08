d8bb4b273127327327dfa1b9b7fbb2c09140bc0c
DaGhostman <ddimitrov.dd@gmail.com>
2019-05-08 18:37:54 +0300

    overall improvements to resolve function

--- src/functions.php
+++ src/functions.php
@@ -2,7 +2,6 @@
 namespace Onion\Framework\Client;
 
 use function Onion\Framework\EventLoop\loop;
-use function Onion\Framework\Promise\async;
 use LibDNS\Decoder\DecoderFactory;
 use LibDNS\Encoder\EncoderFactory;
 use LibDNS\Messages\MessageFactory;
@@ -12,6 +11,7 @@ use LibDNS\Records\Record;
 use LibDNS\Records\ResourceQTypes;
 use Onion\Framework\Promise\Interfaces\PromiseInterface;
 use Onion\Framework\Promise\Promise;
+use function Onion\Framework\EventLoop\coroutine;
 
 if (!function_exists(__NAMESPACE__ . '\resolve')) {
     function resolve(string $domain, string $type, string $server = '1.1.1.1:53'): PromiseInterface {
@@ -21,41 +21,41 @@ if (!function_exists(__NAMESPACE__ . '\resolve')) {
             throw new \InvalidArgumentException("Unknown record type '{$type}'");
         }
 
-        try {
-            $question = (new QuestionFactory)->create($type);
-            $question->setName($domain);
+        return new Promise(function (callable $resolve, callable $reject) use ($domain, $type, $server) {
+            coroutine(function ($resolve, $reject, $domain, $type, $server) {
+                try {
+                    $question = (new QuestionFactory)->create($type);
+                    $question->setName($domain);
 
-            $request = (new MessageFactory)->create(MessageTypes::QUERY);
-            $request->getQuestionRecords()->add($question);
-            $request->isRecursionDesired(true);
+                    $request = (new MessageFactory)->create(MessageTypes::QUERY);
+                    $request->getQuestionRecords()->add($question);
+                    $request->isRecursionDesired(true);
 
-            $packet = (new EncoderFactory)->create()->encode($request);
+                    $client = new Client(Client::TYPE_UDP, $server, 10);
 
-            $client = new Client(Client::TYPE_UDP, $server, 10);
+                    $promise = new Promise(function ($resolve, $reject) use (&$client) {
+                        $client->on('data', function (Packet $packet) use (&$resolve) {
+                            $response = (new DecoderFactory)->create()->decode($packet->read(10 * 1024 * 1024));
 
-            $promise = new Promise(function ($resolve, $reject) use (&$client) {
-                $client->on('data', function (Packet $packet) use (&$resolve) {
-                    $response = (new DecoderFactory)->create()->decode($packet->read(512));
+                            $result = [];
+                            foreach ($response->getAnswerRecords() as $answer) {
+                                /** @var Record $answer */
+                                $result[] = (string) $answer->getData();
+                            }
 
-                    $result = [];
-                    foreach ($response->getAnswerRecords() as $answer) {
-                        /** @var Record $answer */
-                        $result[] = (string) $answer->getData();
-                    }
+                            $resolve($result);
+                        });
 
-                    $resolve($result);
-                });
+                    }, function () {
+                        loop()->tick();
+                    });
+                    $client->send((new EncoderFactory)->create()->encode($request));
 
-            }, function () {
-                loop()->tick();
-            });
-
-            $client->send($packet);
-
-            return $promise;
-        } catch (\Throwable $ex) {
-            var_dump($ex);
-            exit;
-        }
+                    $resolve($promise);
+                } catch (\Throwable $ex) {
+                    $reject($ex);
+                }
+            }, $resolve, $reject, $domain, $type, $server);
+        });
     }
 }
