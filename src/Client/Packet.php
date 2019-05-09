8afe1d2f64cb8e1a856f7507cd983be78d971fc4
DaGhostman <ddimitrov.dd@gmail.com>
2019-05-09 12:28:16 +0300

    formatting

--- src/Client/Packet.php
+++ src/Client/Packet.php
@@ -15,10 +15,12 @@ class Packet
     {
         return stream_socket_recvfrom($this->resource, $size, $flags, $address);
     }
+
     public function send(string $data, string $address, bool $oob = false): int
     {
         return stream_socket_sendto($this->resource, $data, ($oob ? STREAM_OOB : null), $address);
     }
+
     public function getAddress(bool $peer = false)
     {
         return stream_socket_get_name($this->resource, $peer);
